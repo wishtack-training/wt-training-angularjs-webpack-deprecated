b5c817ea7fc72aa9a29048302321f47fd0f47ebc
Younes JAAIDI <yjaaidi@gmail.com>
2016-08-29 18:21:24 +0200

    v0.3.0:     - User list.

--- app/angular/app/app.module.ts
+++ app/angular/app/app.module.ts
@@ -7,7 +7,14 @@
 
 
 import * as angular from 'angular';
-import {UserSelectorComponent} from './user-selector/user-selector.component';
+import {UserListComponent} from './user-list/user-list.component';
+import {UserFormComponent} from './user-form/user-form.component';
+import {UserStore} from './common/user/user-store';
+import {UserPreviewComponent} from './user-preview/user-preview.component';
+
 export const appModule = angular.module('app', []);
 
-appModule.component(UserSelectorComponent.config.name, UserSelectorComponent.config);
+appModule.component('wtUserForm', UserFormComponent.config);
+appModule.component('wtUserList', UserListComponent.config);
+appModule.component('wtUserPreview', UserPreviewComponent.config);
+appModule.service('userStore', UserStore);
